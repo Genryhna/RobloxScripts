6657a0e8b7a9f12729fd180b88b3bdbd1fa9e6bb
Genryhna <votenate@proton.me>
2025-08-07 13:13:32 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,8 @@
+repeat task.wait() until game:IsLoaded()
+if shared.genryhna then return warn("Script already loaded.") end
+
 if game.PlaceId == 15223808363 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Genryhna/RobloxScripts/refs/heads/main/parkour-reborn.lua"))()
-elseif game.PlaceId == 17527914941 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Genryhna/RobloxScripts/refs/heads/main/a-dusty-trip-the-plains.lua"))()
-elseif game.PlaceId == 16389398622 then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Genryhna/RobloxScripts/refs/heads/main/a-dusty-trip-the-desert.lua"))()
-elseif not (game.PlaceId == 15223808363 or game.PlaceId == 16389398622 or game.PlaceId == 17527914941 or game.PlaceId == 17584310335) then
+elseif not (game.PlaceId == 15223808363) then
   return nil
 end
