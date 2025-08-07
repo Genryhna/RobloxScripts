749ea6be7129be8e3920458641d535d78892f947
Genryhna <votenate@proton.me>
2025-08-08 01:33:36 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,10 @@
 repeat task.wait() until game:IsLoaded()
-if shared.genryhna then return warn("Script already loaded.") end
+if shared.genryhna then
+    return warn("Script already loaded.")
+end
 
 if game.PlaceId == 15223808363 then
   loadstring(game:HttpGet("https://raw.githubusercontent.com/Genryhna/RobloxScripts/refs/heads/main/parkour-reborn.lua"))()
-elseif not (game.PlaceId == 15223808363) then
-  return nil
+else
+  return
 end
