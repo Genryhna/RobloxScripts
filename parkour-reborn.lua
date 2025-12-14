3396d2818318d4fe74e60609e7ee0f4e217fcf94
Genryhna <votenate@proton.me>
2025-12-14 14:57:39 +0300

    Update parkour-reborn.lua

--- parkour-reborn.lua
+++ parkour-reborn.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/Genryhna/RobloxScripts/refs/heads/main/loader.lua
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2992ea8679377d64ebea930800b1bc0f.lua"))()
